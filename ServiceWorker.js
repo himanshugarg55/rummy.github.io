271cc73cdbaae3d33ef22eb7c72cd2a2fe38640c
himanshugg <52962891+himanshugg@users.noreply.github.com>
2022-11-17 21:49:04 +0530

    version 22

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Project_RummyGamePlay-0.1";
+const contentToCache = [
+    "Build/rummy.github.io.loader.js",
+    "Build/rummy.github.io.framework.js.unityweb",
+    "Build/rummy.github.io.data.unityweb",
+    "Build/rummy.github.io.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
